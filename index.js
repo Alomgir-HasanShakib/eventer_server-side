0637ab1eaf4b966a9ea53d4f677032837b726260
Shakib <alomgirhasanshakib@gmail.com>
2024-05-11 14:19:34 +0600

    added mongodb connection

--- index.js
+++ index.js
@@ -17,6 +17,41 @@ app.use(express.json());
 
 // mongoDB's code here
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.mmdewqm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+
+    //      my code base here
+    const dataCollection = client.db("eventerDB").collection("allServiceData");
+
+    app.post("/addData", async (req, res) => {
+      const newitem = req.body;
+      console.log(newitem);
+      // const result = await dataCollection.insertOne(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Eventer server is Here");
